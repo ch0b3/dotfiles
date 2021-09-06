b09a955e01948c8718ea4baed4cba461864cf11c
takashi <osakaya555@gmail.com>
2021-09-07 01:27:45 +0900

    Fix symbolic lick

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+set -u
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     echo "$f"
+    ln -snfv "$THIS_DIR"/"$f" ~/
 done
 
 echo "installing Homebrew ..."
