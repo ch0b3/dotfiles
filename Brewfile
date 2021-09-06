8250766350d1c63a1d3677ddfb33a9a6bcc7e1e4
takashi <osakaya555@gmail.com>
2021-09-07 00:21:49 +0900

    Setup brew

---
+++ Brewfile
@@ -0,0 +1,15 @@
+brew "awscli"
+brew "node"
+brew "nodebrew"
+brew "docker"
+brew "openssl@1.1"
+brew "peco"
+brew "starship"
+brew "python@3.9"
+brew "rbenv"
+brew "redis"
+brew "ruby-build"
+
+cask "docker"↲
+cask "ngrok"↲
+cask "dynamodb-local"↲
--- install.sh
+++ install.sh
@@ -7,3 +7,21 @@ do
 
     echo "$f"
 done
+
+echo "installing Homebrew ..."
+which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+cd $THIS_DIR
+
+echo "run brew doctor ..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update ..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade ..."
+brew upgrade
+
+brew bundle
+
+brew cleanup
+
